d9b03e8cf08f1e4649843fef942ddefeae5ccc9a
niclin <nic.lin@abagile.com>
2016-12-16 23:14:25 +0800

    Add user model by devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214141935) do
+ActiveRecord::Schema.define(version: 20161216151243) do
 
   create_table "leave_events", force: :cascade do |t|
     t.date     "start_date"
@@ -33,18 +33,20 @@ ActiveRecord::Schema.define(version: 20161214141935) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "password"
-    t.string   "confirm_password"
-    t.integer  "manager_id"
-    t.date     "joining_date"
-    t.date     "end_date"
-    t.integer  "salary"
-    t.integer  "remaining_leaves"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
