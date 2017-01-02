29c93dc91b1d1950269b9790daf1c7fc5f2833bb
niclin <nic.lin@abagile.com>
2017-01-02 21:06:46 +0800

    Admin can gerenate employee now

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,28 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.all
   end
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :role,
+      :password, :password_confirmation,
+      :join_date, :leave_date
+    )
   end
 end
