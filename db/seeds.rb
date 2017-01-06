965e965d6e6522fcd2ff44cf6e62395e84b84043
niclin <nic.lin@abagile.com>
2017-01-06 22:17:24 +0800

    rubocop style update and fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 u = User.new
-u.email = "admin@test.com"
-u.password = "12345678"
-u.password_confirmation = "12345678"
-u.role = "admin"
-u.name = "Admin"
+u.email = admin@test.com
+u.password = '12345678'
+u.password_confirmation = '12345678'
+u.role = 'admin'
+u.name = 'Admin'
 u.save
