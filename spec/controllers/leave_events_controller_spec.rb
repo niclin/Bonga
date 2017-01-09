d010cc51f28602b69204b1a5bc97afe4128b5a08
niclin <nic.lin@abagile.com>
2017-01-09 10:40:50 +0800

    Using 'let' to refactor user create

--- spec/controllers/leave_events_controller_spec.rb
+++ spec/controllers/leave_events_controller_spec.rb
@@ -2,103 +2,96 @@ require 'rails_helper'
 
 RSpec.describe LeaveEventsController do
   describe "GET index" do
-    it "assigns @leave_events and render template when current user is admin" do
+    context "when admin user login" do
+      let(:user) { create(:user) }
+      it "assigns @leave_events and render template when current user is admin" do
+        sign_in user
 
-      user = create(:user)
-      sign_in user
+        le1 = create(:leave_event)
+        le2 = create(:leave_event)
 
-      le1 = create(:leave_event)
-      le2 = create(:leave_event)
+        get :index
 
-      get :index
+        expect(assigns[:leave_events]).to eq([le1, le2])
+      end
 
-      expect(assigns[:leave_events]).to eq([le1, le2])
+      it "render template" do
+        sign_in user
+
+        le1 = create(:leave_event)
+        le2 = create(:leave_event)
+
+        get :index
+
+        expect(assigns[:leave_events]).to eq([le1, le2])
+        expect(response).to render_template("index")
+      end
     end
 
-    it "render template" do
+    describe "GET show" do
+      it "assigns @leave_events" do
+        sign_in user
 
-      user = create(:user)
-      sign_in user
+        leave_event = create(:leave_event)
 
-      le1 = create(:leave_event)
-      le2 = create(:leave_event)
+        get :show, params: { id: leave_event.id }
 
-      get :index
+        expect(assigns[:leave_event]).to eq(leave_event)
+      end
 
-      expect(assigns[:leave_events]).to eq([le1, le2])
-      expect(response).to render_template("index")
-    end
-  end
+      it "render template" do
+        sign_in user
 
-  describe "GET show" do
-    it "assigns @leave_events" do
-      user = create(:user)
-      sign_in user
+        leave_event = create(:leave_event)
 
-      leave_event = create(:leave_event)
+        get :show, params: { id: leave_event.id }
 
-      get :show, params: { id: leave_event.id }
-
-      expect(assigns[:leave_event]).to eq(leave_event)
+        expect(response).to render_template("show")
+      end
     end
 
-    it "render template" do
-      user = create(:user)
-      sign_in user
 
-      leave_event = create(:leave_event)
+    describe "GET new" do
+      it "assign @leave_event" do
+        sign_in user
+        leave_event = build(:leave_event)
 
-      get :show, params: { id: leave_event.id }
+        get :new
 
-      expect(response).to render_template("show")
-    end
-  end
+        expect(assigns(:leave_event)).to be_a_new(LeaveEvent)
+      end
 
+      it "render template" do
+        sign_in user
 
-  describe "GET new" do
-    it "assign @leave_event" do
-      user = create(:user)
-      sign_in user
-      leave_event = build(:leave_event)
+        leave_event = build(:leave_event)
 
-      get :new
+        get :new
 
-      expect(assigns(:leave_event)).to be_a_new(LeaveEvent)
+        expect(response).to render_template("new")
+      end
     end
 
-    it "render template" do
-      user = create(:user)
-      sign_in user
+    describe "POST create" do
+      it "create a new leave_event record" do
+        sign_in user
 
-      leave_event = build(:leave_event)
+        leave_event = build(:leave_event)
 
-      get :new
+        expect do
+          post :create, params: { :leave_event => attributes_for(:leave_event) }
+        end.to change{ LeaveEvent.count }.by(1)
+      end
 
-      expect(response).to render_template("new")
-    end
-  end
+      it "redirects to leave_events_path" do
+        sign_in user
 
-  describe "POST create" do
-    it "create a new leave_event record" do
-      user = create(:user)
-      sign_in user
+        leave_event = build(:leave_event)
 
-      leave_event = build(:leave_event)
-
-      expect do
         post :create, params: { :leave_event => attributes_for(:leave_event) }
-      end.to change{ LeaveEvent.count }.by(1)
-    end
 
-    it "redirects to leave_events_path" do
-      user = create(:user)
-      sign_in user
-
-      leave_event = build(:leave_event)
-
-      post :create, params: { :leave_event => attributes_for(:leave_event) }
-
-      expect(response).to redirect_to leave_events_path
+        expect(response).to redirect_to leave_events_path
+      end
     end
   end
 end
