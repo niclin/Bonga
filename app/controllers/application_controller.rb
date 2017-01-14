9f6aa1fff9ec2aaff0e83e682fdf66013a95cdc7
niclin <bboyceo@hotmail.com>
2017-01-14 21:19:34 +0800

    The login screen layout will be presented separately

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   def admin_required
     unless current_user.admin?
       redirect_to "/"
+    end
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  layout 'login'
 
   def index
     @users = User.all
