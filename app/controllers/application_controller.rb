617b5ec86af1e542229bd83f1fec04d42be16892
niclin <bboyceo@hotmail.com>
2017-01-15 22:44:37 +0800

    Add i18n in simple_form select options and toggle button for i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :layout_by_resource
+  before_action :set_locale
+
+  protected
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 
   def admin_required
     unless current_user.admin?
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope "(:locale)", locale: /zh-TW|en/ do
+    devise_for :users
 
-  namespace :admin do
-    resources :users
-  end
-
-  resources :leave_events do
-    member do
-      post :verify
+    namespace :admin do
+      resources :users
     end
+
+    resources :leave_events do
+      member do
+        post :verify
+      end
+    end
+
+    resources :users
+
+    root "welcome#index"
   end
-
-  resources :users
-
-  root "welcome#index"
 end
