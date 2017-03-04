ccad536dbe560014c8c00a3494c7a219e140b285
niclin <bboyceo@hotmail.com>
2017-03-04 17:28:56 +0800

    Leave status and type can translate by I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def t_options(key)
+    I18n.t(key, scope: 'select_options', default: '')
+  end
+
+  def t_a(key)
+    I18n.t(key, scope: 'activerecord.attributes', default: '')
+  end
+
+  def t_option_str(key, code)
+    t_options("#{key}.#{code}").empty? ? code : t_options("#{key}.#{code}")
+  end
 end
