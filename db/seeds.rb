546a2d0920fa1cd8abddec6c8760d7422aae7867
niclin <bboyceo@hotmail.com>
2017-03-10 22:51:52 +0800

    Adjustment db:seed file can automatic generate admin/manger/employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-u = User.new
-u.email = 'admin@test.com'
-u.password = '12345678'
-u.password_confirmation = '12345678'
-u.role = 'admin'
-u.name = 'Admin'
-u.save
+%w[admin employee manager].each do |role|
+  u = User.new
+  u.email = "#{role}admin@test.com"
+  u.password = '12345678'
+  u.password_confirmation = '12345678'
+  u.role = "#{role}"
+  u.name = "#{role}"
+  u.save
 
-puts 'Admin is created'
+  puts "#{role} is created"
+end
