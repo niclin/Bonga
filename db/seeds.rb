853025d5d753dc4fc4d64f44591a2f84243ac3b2
niclin <bboyceo@hotmail.com>
2017-03-10 22:56:53 +0800

    Fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 %w[admin employee manager].each do |role|
   u = User.new
-  u.email = "#{role}admin@test.com"
+  u.email = "#{role}@test.com"
   u.password = '12345678'
   u.password_confirmation = '12345678'
   u.role = "#{role}"
