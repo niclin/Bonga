59ebc744699bd5131cf7aa9c9cdea4bd5158b878
niclin <bboyceo@hotmail.com>
2017-03-11 15:04:29 +0800

    Minor adjustment
    - Remove rule from rubocop.
    - Remove admin dependent code because we using `cancancan` now.
    - Fix rspec test at leave event validate fail.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /zh-TW|en/ do
     devise_for :users
 
-    namespace :admin do
-      resources :users
-    end
-
     resources :leave_events do
       member do
         post :verify
--- spec/models/leave_event_spec.rb
+++ spec/models/leave_event_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe LeaveEvent do
   it { is_expected.to validate_presence_of(:start_time) }
   it { is_expected.to validate_presence_of(:end_time) }
-  it { is_expected.to validate_presence_of(:hours) }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:leave_type) }
 end
